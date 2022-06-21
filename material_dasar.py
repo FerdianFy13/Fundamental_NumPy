258ec2f8b65fbb93ca58a26cd78aa845d4842422
Ferdianfy13 <ferdianfy13@gmail.com>
2022-06-21 19:35:36 +0700

    NumPy Fundamental Start

---
+++ material_dasar.py
@@ -0,0 +1,10 @@
+# data analysis adalah proses yang dilakukan untuk mengambil data dan memprosesnya
+# dalam python terdapat beberapa package data komputasi yang bisa digunakan antara lain pandas, numpy, scipy, matplotlib
+# numpy adalah package yang digunakan untuk melakukan perhitungan matematika
+class Data:
+    def __init__(self, name):
+        self.name = name
+
+
+Ferdi = Data("Ferdi")
+print(Ferdi.name)
