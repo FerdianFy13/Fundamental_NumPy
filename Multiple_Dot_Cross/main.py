6be31a16142561a80ce534e22588a8b01c69e1a9
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-24 14:36:54 +0700

    Multiple Cross and Dot Matrix

---
+++ Multiple_Dot_Cross/main.py
@@ -0,0 +1,10 @@
+# perkalian dot adalah perkalian antara besarnya a dengan besarnya b
+import numpy as np
+
+a = np.array([1, 2])
+b = np.array([3, 4])
+
+c = np.dot(a, b)
+d = np.cross(a, b)
+print(c)
+print(d)
